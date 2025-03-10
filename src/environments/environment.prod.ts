746bdc7e1eef5e57ccccf2abb56da044ef519976
demmersdylan <dylan@mozaicsoftware.com>
2025-03-09 19:23:25 -0700

    fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/',  // This will be relative to your Static Web App's domain
+  apiUrl: '/api',  // This will be relative to your Static Web App's domain
   hubspot: {
     portalId: '44444444',
     formId: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
